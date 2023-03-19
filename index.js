312cac5948c2bf79ccacd770cc2714662feb4ed7
John <johnkaranjairungu@gmail.com>
2023-03-19 19:17:15 +0300

    Completed the assignment

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-// Code your solutions in this file
+const names = ["Guadalupe", "Ollie", "Aki"];
+function writeCards(names) {
+    for (let i = 0; i < names.length; i++) {
+        console.log("Wrapped" + names[i] + ", and added a bow!");
+        debugger
+    }
+}
+
+countDown(10);
